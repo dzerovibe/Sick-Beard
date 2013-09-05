0f589e4b7a26d2f1b6b0f62d032204893977ce84
Jason Lane <contact@jason-lane.com>
2013-09-05 21:36:06 +1000

    Modified Pistachitos version checking to use my fork instead (DZV-Dev)

--- sickbeard/versionChecker.py
+++ sickbeard/versionChecker.py
@@ -114,7 +114,7 @@ class CheckVersion():
 class UpdateManager():
 
     def get_github_repo_user(self):
-        return 'mr-orange'
+        return 'dzerovibe'
 
     def get_github_repo(self):
         return 'Sick-Beard'
